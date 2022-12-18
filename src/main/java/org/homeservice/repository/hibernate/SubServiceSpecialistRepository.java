4f122a4f54d20953be662eb40f12d4dc73225dac
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-18 23:58:51 +0330

    Create SubServiceSpecialistRepository and SubServiceSpecialistRepositoryImpl in hibernate package.

---
+++ src/main/java/org/homeservice/repository/hibernate/SubServiceSpecialistRepository.java
@@ -0,0 +1,8 @@
+package org.homeservice.repository.hibernate;
+
+import org.homeservice.entity.SubServiceSpecialist;
+import org.homeservice.entity.id.SubServiceSpecialistId;
+import org.homeservice.repository.hibernate.base.BaseRepository;
+
+public interface SubServiceSpecialistRepository extends BaseRepository<SubServiceSpecialist, SubServiceSpecialistId> {
+}
