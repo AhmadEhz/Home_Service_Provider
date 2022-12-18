4c3b335fbadf5b852b068a2f9c2965549573b162
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-18 19:10:48 +0330

    Add findByName() and isExistedName() to ServiceService. Override save() of ServiceServiceImpl. (check name should be unique)

--- src/main/java/org/homeservice/service/ServiceService.java
+++ src/main/java/org/homeservice/service/ServiceService.java
@@ -3,6 +3,11 @@ package org.homeservice.service;
 import org.homeservice.entity.Service;
 import org.homeservice.service.base.BaseService;
 
+import java.util.Optional;
+
 @org.springframework.stereotype.Service
 public interface ServiceService extends BaseService<Service, Long> {
+    Optional<Service> findByName(String name);
+
+    boolean isExistedName(String name);
 }
--- src/main/java/org/homeservice/service/impl/ServiceServiceImpl.java
+++ src/main/java/org/homeservice/service/impl/ServiceServiceImpl.java
@@ -4,12 +4,32 @@ import org.homeservice.entity.Service;
 import org.homeservice.repository.ServiceRepository;
 import org.homeservice.service.ServiceService;
 import org.homeservice.service.base.BaseServiceImpl;
+import org.homeservice.util.exception.NonUniqueException;
 import org.springframework.context.annotation.Scope;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 @Scope("singleton")
-public class ServiceServiceImpl extends BaseServiceImpl<Service,Long, ServiceRepository> implements ServiceService {
+public class ServiceServiceImpl extends BaseServiceImpl<Service, Long, ServiceRepository> implements ServiceService {
     public ServiceServiceImpl(ServiceRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public void save(Service service) {
+        if (isExistedName(service.getName()))
+            throw new NonUniqueException("Service name is exist.");
+        super.save(service);
+    }
+
+    @Override
+    public Optional<Service> findByName(String name) {
+        return repository.findServiceByName(name);
+    }
+
+    @Override
+    public boolean isExistedName(String name) {
+        return findByName(name).isPresent();
     }
 }
