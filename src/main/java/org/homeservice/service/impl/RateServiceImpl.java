d73a8bc911209bf4928a5ae277ae386f10ce5cb2
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-18 17:53:25 +0330

    Override save() of RateServiceImpl. (Update Specialist score after saving Rate)

--- src/main/java/org/homeservice/service/impl/RateServiceImpl.java
+++ src/main/java/org/homeservice/service/impl/RateServiceImpl.java
@@ -3,10 +3,21 @@ package org.homeservice.service.impl;
 import org.homeservice.entity.Rate;
 import org.homeservice.repository.RateRepository;
 import org.homeservice.service.RateService;
+import org.homeservice.service.SpecialistService;
 import org.homeservice.service.base.BaseServiceImpl;
 
 public class RateServiceImpl extends BaseServiceImpl<Rate, Long, RateRepository> implements RateService {
-    protected RateServiceImpl(RateRepository repository) {
+
+    SpecialistService specialistService;
+
+    public RateServiceImpl(RateRepository repository, SpecialistService specialistService) {
         super(repository);
+        this.specialistService = specialistService;
+    }
+
+    @Override
+    public void save(Rate rate) {
+        super.save(rate);
+        specialistService.updateScoreByRateId(rate.getId());
     }
 }
