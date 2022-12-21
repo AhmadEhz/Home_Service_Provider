4f9bcfa46cbbb5ed03ab436eb74f91a330b07807
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-21 17:31:09 +0330

    Add constructor to Admin.

--- src/main/java/org/homeservice/entity/Admin.java
+++ src/main/java/org/homeservice/entity/Admin.java
@@ -4,5 +4,10 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Admin extends Person {
+    public Admin() {
+    }
 
+    public Admin(String firstName, String lastName, String username, String password) {
+        super(firstName, lastName, username, password);
+    }
 }
