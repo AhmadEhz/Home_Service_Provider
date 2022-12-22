ed52508888e73c4b6fed380ee6650f60a0c7e438
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-22 18:03:24 +0330

    Add addService() and addSubService() to AdminTest.

--- src/test/java/org/homeservice/AdminTest.java
+++ src/test/java/org/homeservice/AdminTest.java
@@ -1,11 +1,10 @@
 package org.homeservice;
 
 import org.homeservice.entity.Admin;
+import org.homeservice.entity.Service;
+import org.homeservice.entity.SubService;
 import org.homeservice.util.exception.CustomIllegalArgumentException;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
@@ -25,9 +24,21 @@ public class AdminTest {
         this.services = services;
     }
 
+    static Service service1 = new Service("service1Name");
+    static Service service2 = new Service("service2Name");
+    static Service service3 = new Service("service3Name");
+    static SubService subService1 = new SubService
+            ("sub1forServ1", "sub1description", 200D, service1);
+    static SubService subService2 = new SubService(
+            "sub2forServ2", "sub2description", 250D, service2);
+    static SubService subService3 = new SubService
+            ("sub3forServ2", "sub3description", 225D, service2);
+    static SubService subService4 = new SubService
+            ("sub4forServ3", "sub4description", 150D, service3);
+
     static Admin admin1 = new Admin("adminFirstName", "adminLastName"
             , "AdminUsername", "AdminPassword");
-
+    
     @Test
     @Order(1)
     void save() {
@@ -50,5 +61,33 @@ public class AdminTest {
         String password = services.adminService.findById(admin1.getId()).get().getPassword() + "ab";
         assertThrows(CustomIllegalArgumentException.class, () ->
                 services.adminService.changePassword(admin1.getUsername(), password, "newPass"));
+    }
+
+    @Test
+    @Order(3)
+    void addService() {
+        services.serviceService.save(service1);
+        services.serviceService.save(service2);
+        services.serviceService.save(service3);
+        assertEquals(service1, services.serviceService.findById(service1.getId()).get());
+        assertEquals(service2, services.serviceService.findById(service2.getId()).get());
+        assertEquals(service3, services.serviceService.findById(service3.getId()).get());
+        assertEquals(service1, services.serviceService.findByName(service1.getName()).get());
+    }
+
+    @Test
+    @Order(4)
+    void addSubService() {
+        services.subServiceService.save(subService1);
+        services.subServiceService.save(subService2);
+        services.subServiceService.save(subService3);
+        services.subServiceService.save(subService4);
+        assertEquals(subService1, services.subServiceService.findById(subService1.getId()).get());
+        assertEquals(subService2, services.subServiceService.findById(subService2.getId()).get());
+        assertEquals(subService3, services.subServiceService.findById(subService3.getId()).get());
+        assertEquals(subService4, services.subServiceService.findById(subService4.getId()).get());
+        service2 = services.serviceService.findById(service2.getId()).get();
+        assertEquals(service2.getSubServices().size(),
+                services.subServiceService.loadAllByService(service2.getId()).size());
     }
 }
