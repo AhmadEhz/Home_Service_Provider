374e18e9060709e6cb9f679386779dfa32b53651
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-23 02:09:19 +0330

    Add loadById() to Service in test scope. Make final all fields.

--- src/test/java/org/homeservice/Services.java
+++ src/test/java/org/homeservice/Services.java
@@ -1,5 +1,6 @@
 package org.homeservice;
 
+import org.homeservice.entity.*;
 import org.homeservice.service.*;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -7,19 +8,20 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("singleton")
 public class Services {
-     AdminService adminService;
-     CustomerService customerService;
-     SpecialistService specialistService;
-     OrderService orderService;
-     BidService bidService;
-     RateService rateService;
-     ServiceService serviceService;
-     SubServiceService subServiceService;
+    final AdminService adminService;
+    final CustomerService customerService;
+    final SpecialistService specialistService;
+    final OrderService orderService;
+    final BidService bidService;
+    final RateService rateService;
+    final ServiceService serviceService;
+    final SubServiceService subServiceService;
+    final SubServiceSpecialistService subServiceSpecialistService;
 
-    public Services( AdminService adminService, CustomerService customerService,
-                     SpecialistService specialistService, OrderService orderService,
-                     BidService bidService, RateService rateService, ServiceService serviceService,
-                     SubServiceService subServiceService) {
+    public Services(AdminService adminService, CustomerService customerService,
+                    SpecialistService specialistService, OrderService orderService,
+                    BidService bidService, RateService rateService, ServiceService serviceService,
+                    SubServiceService subServiceService, SubServiceSpecialistService subServiceSpecialistService) {
         this.adminService = adminService;
         this.customerService = customerService;
         this.specialistService = specialistService;
@@ -28,6 +30,28 @@ public class Services {
         this.rateService = rateService;
         this.serviceService = serviceService;
         this.subServiceService = subServiceService;
+        this.subServiceSpecialistService = subServiceSpecialistService;
     }
 
+    Order loadOrder(Long orderId) {
+        return orderService.findById(orderId).get();
+    }
+    Bid loadBid(Long bidId) {
+        return bidService.findById(bidId).get();
+    }
+    Specialist loadSpecialist(Long specialistId) {
+        return specialistService.findById(specialistId).get();
+    }
+    Customer loadCustomer(Long customerId) {
+        return customerService.findById(customerId).get();
+    }
+    Admin loadAdmin(Long adminId) {
+        return adminService.findById(adminId).get();
+    }
+    Service loadService(Long serviceId) {
+        return serviceService.findById(serviceId).get();
+    }
+    SubService loadSubService(Long subServiceId) {
+        return subServiceService.findById(subServiceId).get();
+    }
 }
