74e5a9e5a75e418abd28c19be03c09532b890411
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-24 17:13:55 +0330

    Add save() by Order and Customer in RateService.

--- src/main/java/org/homeservice/service/RateService.java
+++ src/main/java/org/homeservice/service/RateService.java
@@ -3,7 +3,9 @@ package org.homeservice.service;
 import org.homeservice.entity.Rate;
 import org.homeservice.service.base.BaseService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public interface RateService extends BaseService<Rate, Long> {
+    void save(Rate rate, Long orderId, Long customerId);
 }
--- src/main/java/org/homeservice/service/impl/RateServiceImpl.java
+++ src/main/java/org/homeservice/service/impl/RateServiceImpl.java
@@ -1,10 +1,15 @@
 package org.homeservice.service.impl;
 
+import org.homeservice.entity.Customer;
+import org.homeservice.entity.Order;
 import org.homeservice.entity.Rate;
 import org.homeservice.repository.RateRepository;
+import org.homeservice.service.CustomerService;
+import org.homeservice.service.OrderService;
 import org.homeservice.service.RateService;
 import org.homeservice.service.SpecialistService;
 import org.homeservice.service.base.BaseServiceImpl;
+import org.homeservice.util.exception.NotFoundException;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,17 +18,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Scope("singleton")
 public class RateServiceImpl extends BaseServiceImpl<Rate, Long, RateRepository> implements RateService {
 
-    SpecialistService specialistService;
+    private final SpecialistService specialistService;
+    private final OrderService orderService;
+    private final CustomerService customerService;
 
-    public RateServiceImpl(RateRepository repository, SpecialistService specialistService) {
+    public RateServiceImpl(RateRepository repository, SpecialistService specialistService,
+                           OrderService orderService, CustomerService customerService) {
         super(repository);
         this.specialistService = specialistService;
+        this.orderService = orderService;
+        this.customerService = customerService;
     }
 
     @Override
     @Transactional
     public void save(Rate rate) {
+        if(rate.getOrder()==null||rate.getOrder().getId()==null)
+            throw new NullPointerException("Order or OrderId is null.");
         super.save(rate);
         specialistService.updateScoreByRateId(rate.getId());
+    }
+
+    @Override
+    @Transactional
+    public void save(Rate rate, Long orderId, Long customerId) {
+        Order order = orderService.findById(orderId).orElseThrow(() -> new NotFoundException("Order not found."));
+        Customer customer = customerService.findById(customerId).orElseThrow(
+                () -> new NotFoundException("Customer not found."));
+        if(!order.getCustomer().equals(customer))
+            throw new IllegalArgumentException("This Order is not for this Customer.");
+
+        rate.setOrder(order);
+        save(rate);
     }
 }
