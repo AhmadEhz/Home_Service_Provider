b732e7f0097ff1bd742bad23b40d5986cece1749
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-25 12:02:49 +0330

    Add save(), changePassword(), addAvatar() and saveBid() to SpecialistController.

--- src/main/java/org/homeservice/controller/SpecialistController.java
+++ src/main/java/org/homeservice/controller/SpecialistController.java
@@ -1,10 +1,16 @@
 package org.homeservice.controller;
 
+import org.homeservice.dto.BidDTO;
+import org.homeservice.entity.Specialist;
 import org.homeservice.service.BidService;
 import org.homeservice.service.SpecialistService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/specialist")
 public class SpecialistController {
     private final SpecialistService specialistService;
     private final BidService bidService;
@@ -12,5 +18,25 @@ public class SpecialistController {
     public SpecialistController(SpecialistService specialistService, BidService bidService) {
         this.specialistService = specialistService;
         this.bidService = bidService;
+    }
+
+    @PostMapping("/save")
+    void save(@RequestBody Specialist specialist) {
+        specialistService.save(specialist);
+    }
+
+    @PutMapping("/change-password")
+    void changePassword(Map<String, String> map) {
+        specialistService.changePassword(map.get("username"), map.get("oldPassword"), map.get("newPassword"));
+    }
+
+    @PutMapping("/add-avatar/{id}")
+    void addAvatar(@PathVariable Long id, @RequestBody MultipartFile avatar) {
+        specialistService.addAvatar(id, avatar);
+    }
+
+    @PutMapping ("/set-bid")
+    void saveBid(@RequestBody BidDTO bidDTO) {
+        bidService.save(bidDTO.getBid(), bidDTO.getOrderId(), bidDTO.getSpecialistId());
     }
 }
