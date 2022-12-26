f1147b751424f820187097b0a380341d18def116
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-26 23:55:12 +0330

    Add showOrders() and showOrdersByWaitingStatus() to SpecialistController.

--- src/main/java/org/homeservice/controller/SpecialistController.java
+++ src/main/java/org/homeservice/controller/SpecialistController.java
@@ -1,12 +1,17 @@
 package org.homeservice.controller;
 
 import org.homeservice.dto.BidCreationDto;
+import org.homeservice.dto.OrderDto;
+import org.homeservice.dto.SpecialistCreationDto;
+import org.homeservice.entity.Order;
 import org.homeservice.entity.Specialist;
 import org.homeservice.service.BidService;
+import org.homeservice.service.OrderService;
 import org.homeservice.service.SpecialistService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,15 +19,29 @@ import java.util.Map;
 public class SpecialistController {
     private final SpecialistService specialistService;
     private final BidService bidService;
+    private final OrderService orderService;
 
-    public SpecialistController(SpecialistService specialistService, BidService bidService) {
+    public SpecialistController(SpecialistService specialistService, BidService bidService, OrderService orderService) {
         this.specialistService = specialistService;
         this.bidService = bidService;
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/order/showAll")
+    List<OrderDto> showOrders(@RequestParam("id") Long specialistId) {
+        List<Order> orders = orderService.loadAllBySpecialistSubServices(specialistId);
+        return OrderDto.convertToDto(orders);
+    }
+
+    @GetMapping("/order/showAll-waiting-for-bid")
+    List<OrderDto> showOrdersByWaitingStatus(@RequestParam("id") Long specialistId) {
+        List<Order> orders = orderService.loadAllByWaitingStatusAndSpecialist(specialistId);
+        return OrderDto.convertToDto(orders);
     }
 
     @PostMapping("/save")
-    void save(@RequestBody Specialist specialist) {
-        specialistService.save(specialist);
+    void save(@RequestBody SpecialistCreationDto specialistDto) {
+        specialistService.save(specialistDto.getSpecialist());
     }
 
     @PutMapping("/change-password")
