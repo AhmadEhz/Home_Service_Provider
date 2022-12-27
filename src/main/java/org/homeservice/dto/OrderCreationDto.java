7604e35fb41baf77f428d1624842b185fe71dd05
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-27 14:20:40 +0330

    Add @ JsonFormat to LocalDateTime in OrderCreationDto.

--- src/main/java/org/homeservice/dto/OrderCreationDto.java
+++ src/main/java/org/homeservice/dto/OrderCreationDto.java
@@ -1,5 +1,6 @@
 package org.homeservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.homeservice.entity.Order;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 public class OrderCreationDto {
     private Double offerPrice;
     private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime workingTime;
     private String address;
     private Long customerId;
