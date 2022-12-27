ce083b3c533df9de33d14e105a8dcca502418670
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-27 17:28:01 +0330

    Create SubServiceEditionDto.

---
+++ src/main/java/org/homeservice/dto/SubServiceEditionDto.java
@@ -0,0 +1,12 @@
+package org.homeservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubServiceEditionDto {
+    private Long id;
+    private String description;
+    private Long basePrice;
+}
