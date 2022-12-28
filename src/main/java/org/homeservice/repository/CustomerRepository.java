0a4dc8634f168866a82fdae9eda3ca0fcb0e167a
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-28 19:56:46 +0330

    Extend CustomerRepository from JpaSpecificationExecutor.

--- src/main/java/org/homeservice/repository/CustomerRepository.java
+++ src/main/java/org/homeservice/repository/CustomerRepository.java
@@ -2,12 +2,13 @@ package org.homeservice.repository;
 
 import org.homeservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
 
     Optional<Customer> findCustomerByUsername(String username);
 
