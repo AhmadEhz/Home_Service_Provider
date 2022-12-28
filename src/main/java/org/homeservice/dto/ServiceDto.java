b55b0961d1564b6179c70732d1ff923615e0085e
Ahmad_E <Ahmad_Ehz@yahoo.com>
2022-12-28 19:45:34 +0330

    Create ServiceDto.

---
+++ src/main/java/org/homeservice/dto/ServiceDto.java
@@ -0,0 +1,32 @@
+package org.homeservice.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.homeservice.entity.Service;
+import org.homeservice.entity.SubService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class ServiceDto {
+    private Long id;
+    private String name;
+
+    public ServiceDto() {
+    }
+
+    public ServiceDto(Service service) {
+        id = service.getId();
+        name = service.getName();
+    }
+
+    public static List<ServiceDto> convertToDto(List<Service> services) {
+        List<ServiceDto> serviceDtoList = new ArrayList<>();
+        for (Service s : services)
+            serviceDtoList.add(new ServiceDto(s));
+        return serviceDtoList;
+    }
+}
