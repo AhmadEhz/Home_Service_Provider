30609242d281c296297a6ff8498dbbdeed9a8383
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-07 14:33:30 +0330

    Add initial value to OrderStatus. Add constructor and getValue() to OrderStatus.

--- src/main/java/org/homeservice/entity/OrderStatus.java
+++ src/main/java/org/homeservice/entity/OrderStatus.java
@@ -1,10 +1,18 @@
 package org.homeservice.entity;
 
 public enum OrderStatus {
-    WAITING_FOR_BID,
-    WAITING_FOR_CHOOSE_SPECIALIST,
-    WAITING_FOR_COMING_SPECIALIST,
-    STARTED,
-    FINISHED,
-    PAID
+    WAITING_FOR_BID("waitingforbid"),
+    WAITING_FOR_CHOOSE_SPECIALIST("waitingforchoosespecialist"),
+    WAITING_FOR_COMING_SPECIALIST("waitingforcomingspecialist"),
+    STARTED("started"),
+    FINISHED("finished"),
+    PAID("paid");
+
+    private final String value;
+    OrderStatus(String name) {
+        this.value = name.toLowerCase();
+    }
+    public String getValue() {
+        return value;
+    }
 }
