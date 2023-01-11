a2c17631a84eadc4e37ffd0123be2dc9bf8ae039
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-11 04:38:25 +0330

    Add sendSimpleMessage() for sneding email using api.

--- src/main/java/org/homeservice/util/EmailSender.java
+++ src/main/java/org/homeservice/util/EmailSender.java
@@ -1,8 +1,11 @@
 package org.homeservice.util;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class EmailSender {
@@ -19,11 +22,26 @@ public class EmailSender {
 
     public void sendVerifyingEmail(String to, String verificationCode, EmailFor emailFor) {
         SimpleMailMessage mail = new SimpleMailMessage();
-        mail.setFrom("noreply@localhost");
+        mail.setFrom("noreply@gamil.com");
         mail.setTo(to);
         mail.setSubject(subject);
         mail.setText(message + url + emailFor.value + param + verificationCode);
         emailSender.send(mail);
+    }
+
+    public void sendSimpleMessage(String to, String verificationCode, EmailFor emailFor) {
+        try {
+            Unirest.post("https://api.mailgun.net/v3/sandbox6a29fe44516146a3827e305ab7524936.mailgun.org"
+                         + "/messages").
+                    basicAuth("api", "f09ad9181a7d5502d82502fd9c1ba18f-4c2b2223-4fce5800")
+                    .queryString("from", "noreply@localhost")
+                    .queryString("to", to)
+                    .queryString("subject", subject)
+                    .queryString("text", message + url + emailFor.value + param + verificationCode)
+                    .asJson();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public enum EmailFor {
