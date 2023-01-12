ff510285017916deb5dad6268faff4885c319147
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-13 00:05:24 +0330

    Add @ Service and @ Scope(singleton) to PersonServiceImpl.

--- src/main/java/org/homeservice/service/impl/PersonServiceImpl.java
+++ src/main/java/org/homeservice/service/impl/PersonServiceImpl.java
@@ -4,9 +4,13 @@ import org.homeservice.entity.Person;
 import org.homeservice.repository.PersonRepository;
 import org.homeservice.service.PersonService;
 import org.homeservice.service.base.BaseServiceImpl;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@Scope("singleton")
 public class PersonServiceImpl extends BaseServiceImpl<Person,Long, PersonRepository> implements PersonService {
     protected PersonServiceImpl(PersonRepository repository) {
         super(repository);
