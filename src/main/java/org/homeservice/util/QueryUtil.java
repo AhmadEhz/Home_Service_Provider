bcaf9fb15220ba90275757d0cd7a71e32d94ec66
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-12 14:48:53 +0330

    Remove unsed methods.

--- src/main/java/org/homeservice/util/QueryUtil.java
+++ src/main/java/org/homeservice/util/QueryUtil.java
@@ -34,25 +34,13 @@ public class QueryUtil {
     }
 
     private static class Values {
-        static final Map<String, String> filterValues;
+
         static final Map<String, Sort> sortValues;
 
         static {
-            filterValues = new HashMap<>();
             sortValues = new HashMap<>();
-            filterValues.put("firstname", "firstName");
-            filterValues.put("lastname", "lastName");
-            filterValues.put("score", "score");
-            filterValues.put("email", "email");
-            filterValues.put("subserviceid", "subServiceId");
             sortValues.put("price", Sort.by(Sort.Direction.ASC, "offerPrice"));
             sortValues.put("specialist", Sort.by(Sort.Direction.DESC, "specialist.score"));
-        }
-
-
-
-        static String getFilter(String key) {
-            return filterValues.get(key.toLowerCase());
         }
 
         static Sort getSort(String key) {
