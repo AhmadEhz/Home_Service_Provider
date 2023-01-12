29973b89562d2ec118207d0eba775c7c3d0943a4
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-12 14:49:37 +0330

    Remove initial value from OrderStatus.

--- src/main/java/org/homeservice/entity/OrderStatus.java
+++ src/main/java/org/homeservice/entity/OrderStatus.java
@@ -1,18 +1,10 @@
 package org.homeservice.entity;
 
 public enum OrderStatus {
-    WAITING_FOR_BID("waitingforbid"),
-    WAITING_FOR_CHOOSE_SPECIALIST("waitingforchoosespecialist"),
-    WAITING_FOR_COMING_SPECIALIST("waitingforcomingspecialist"),
-    STARTED("started"),
-    FINISHED("finished"),
-    PAID("paid");
-
-    private final String value;
-    OrderStatus(String name) {
-        this.value = name.toLowerCase();
-    }
-    public String getValue() {
-        return value;
-    }
+    WAITING_FOR_BID,
+    WAITING_FOR_CHOOSE_SPECIALIST,
+    WAITING_FOR_COMING_SPECIALIST,
+    STARTED,
+    FINISHED,
+    PAID
 }
