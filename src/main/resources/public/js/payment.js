21e8ffc154d8a6b7aa0bff29074858336f0de9a9
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-13 00:09:17 +0330

    Remove commented and unnecessary codes.

--- src/main/resources/public/js/payment.js
+++ src/main/resources/public/js/payment.js
@@ -1,25 +1,14 @@
 function submitForm() {
     var xhr = new XMLHttpRequest();
-    var forms = document.getElementById("forms");
-    const form = {
+    const paymentForm = {
         cardNumber: document.getElementById("card-number").value,
         cvv2: document.getElementById("cvv2").value,
         expireDate: document.getElementById("expire-date").value,
         captcha: grecaptcha.getResponse()
-
     };
-    // if (xhr.readyState === 4 && xhr.status === 200) {
-
-    //     // Print received data from server
-    //     result.innerHTML = this.responseText;
-
-    // }
-
 xhr.open("POST", "http://localhost:8080/customer/payment", true);
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("Access-Control-Allow-Origin","*");
-var data = JSON.stringify(form);
-console.log(data);
-xhr.send(data);
-// return false;
+var paymentJson = JSON.stringify(paymentForm);
+console.log(paymentJson);
+xhr.send(paymentJson);
 }
