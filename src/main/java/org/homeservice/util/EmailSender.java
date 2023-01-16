0ce2c758a72d84be722eeaa4612b41c521be6a7d
Ahmad_E <Ahmad_Ehz@yahoo.com>
2023-01-16 16:08:06 +0330

    Fix sending email.

--- src/main/java/org/homeservice/util/EmailSender.java
+++ src/main/java/org/homeservice/util/EmailSender.java
@@ -1,11 +1,10 @@
 package org.homeservice.util;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.homeservice.util.exception.ConnectionBrokenException;
-//import org.homeservice.util.exception.CustomIllegalArgumentException;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 
@@ -15,27 +14,13 @@ public class EmailSender {
     private static final String message =
             "To verify your email, please click on link below:\n http://localhost:8080/%s/verifyEmail?verify=%s";
 
-//    private final JavaMailSender emailSender;
-//
-//    public EmailSender(JavaMailSender emailSender) {
-//        this.emailSender = emailSender;
-//    }
-//
-//    public void sendVerifyingEmail(String to, String verificationCode, EmailFor emailFor) {
-//        SimpleMailMessage mail = new SimpleMailMessage();
-//        mail.setFrom("noreply@gamil.com");
-//        mail.setTo(to);
-//        mail.setSubject(subject);
-//        mail.setText(String.format(message, emailFor.value, verificationCode));
-//        emailSender.send(mail);
-//    }
-
-    public void sendSimpleMessage(String to, String verificationCode, EmailFor emailFor) {
+    public void send(String to, String verificationCode, EmailFor emailFor) {
+        HttpResponse<JsonNode> response = null;
         try {
-            Unirest.post("https://api.mailgun.net/v3/sandbox6a29fe44516146a3827e305ab7524936.mailgun.org"
-                         + "/messages").
+            response = Unirest.post(
+                    "https://api.mailgun.net/v3/sandbox6a29fe44516146a3827e305ab7524936.mailgun.org/messages").
                     basicAuth("api", "f09ad9181a7d5502d82502fd9c1ba18f-4c2b2223-4fce5800")
-                    .queryString("from", "sandbox6a29fe44516146a3827e305ab7524936.mailgun.org")
+                    .queryString("from", "mailgun@sandbox6a29fe44516146a3827e305ab7524936.mailgun.org")
                     .queryString("to", to)
                     .queryString("subject", subject)
                     .queryString("text", String.format(message, emailFor.value, verificationCode))
